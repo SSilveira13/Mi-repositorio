b38f7dccea21f48dbf3f1c0dfc135eb5ea149d7f
Santiago Silveira <36273222+SSilveira13@users.noreply.github.com>
2018-04-09 21:44:16 -0300

    Add files via upload

---
+++ StringAvanzado/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char nombre[31];
+    char apellido[31];
+    char NombreyApellido[61];
+
+    printf("Nombre: ");
+    fflush(stdin);
+    scanf("%s",nombre);
+    strlwr(nombre);
+    nombre[0]=toupper(nombre[0]);
+
+    printf("Apellido: ");
+    fflush(stdin);
+    scanf("%s",apellido);
+    strlwr(apellido);
+    apellido[0]=toupper(apellido[0]);
+
+    strcpy(NombreyApellido,nombre);
+    strcat(NombreyApellido," ");
+    strcat(NombreyApellido,apellido);
+    printf("Nombre y apellido ingresado: %s",NombreyApellido);
+    return 0;
+}
