305112de8972b0b4a30d88718f9398674706584f
Santiago Silveira <36273222+SSilveira13@users.noreply.github.com>
2018-06-18 21:54:44 -0300

    Add files via upload

---
+++ cosas para el segundo parcial/arraylist/examples/example_4/src/main.c
@@ -0,0 +1,55 @@
+/*
+    utest example : Unit test examples.
+    Copyright (C) <2016>  <Mauricio Davila>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../testing/inc/main_test.h"
+#include "../inc/ArrayList.h"
+#include "../inc/Employee.h"
+
+
+
+int main(void)
+{
+        startTesting(1);//Funciona newArrayList
+        startTesting(2);//Funciona add
+        //startTesting(3);//Funciona deleteArrayList preguntar------------------
+        startTesting(4);//Funciona len
+        startTesting(5);//Funciona get
+        startTesting(6);//Funciona contains
+        startTesting(7);//Funciona set
+        startTesting(8);//Funciona Remove
+        startTesting(9);//Funciona clear
+        //startTesting(10);//NO Funciona clone
+        //startTesting(11);//No funciona push-------------------
+        startTesting(12);//Funciona indexOf
+        startTesting(13);//Funciona isEmpty
+        startTesting(14);//No funciona revisar pop 80%------------
+        //startTesting(15);// subList-----------------------
+        //startTesting(16);//Funciona containsAll
+        //startTesting(17);// sort---------------------------
+
+    return 0;
+}
+
+
+
+
+
